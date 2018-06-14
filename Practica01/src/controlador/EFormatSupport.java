5c23ad8ebb6c3274b93f2691892ec1407946fad2
Shally Gojjra <Shally.gojjra@icloud.com>
2018-06-14 14:24:07 +0200

    Práctica 01 | ING INF

--- Practica01/src/controlador/EFormatSupport.java
+++
@@ -1,5 +0,0 @@
-package controlador;
-
-public enum EFormatSupport {
-    STRING,DATE,DECIMAL,INTEGER,TIME
-}
--- Practica01/src/controlador/ValidadorDeFormato.java
+++
@@ -1,8 +0,0 @@
-package controlador;
-
-public interface ValidadorDeFormato {
-    
-    public boolean formatoCorrecto(String strValor);
-    
-    public String getError();
-}
